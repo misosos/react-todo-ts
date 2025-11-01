6cb436792dd9f56d1ca55ae0cca6a395191ed487
miso <misomiso0307@gmail.com>
2025-11-01 21:28:15 +0900

    App.tsx 업뎃

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState,useRef} from 'react';
+import TodoTemplate from './components/TodoTemplate';
+import TodoList from './components/TodoList';
+import TodoInsert from './components/TodoInsert';
 
-function App() {
-  const [count, setCount] = useState(0)
+export type Todo = {
+    id: number;
+    text: string;
+    done: boolean;
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function App() {
+    const [todos, setTodos] = useState<Todo[]>([]);
+    const nextId = useRef(1);
+    const handleAdd = (text: string) => {
+        setTodos((prev) => [...prev, {
+            id: nextId.current++,
+            text, done: false
+        }]);
+    };
+
+    return (
+        <TodoTemplate>
+            <h1>Todo List</h1>
+            <TodoInsert onAdd={handleAdd}/>
+            <TodoList todos={todos}/>
+        </TodoTemplate>
+    );
 }
-
-export default App
