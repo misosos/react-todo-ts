7ca25ad1c29e22385586f648e2e0e943cb2bd842
miso <misomiso0307@gmail.com>
2025-11-01 21:54:14 +0900

    TodoInsert 컴포넌트 업뎃

---
+++ src/components/TodoInsert.tsx
@@ -0,0 +1,29 @@
+import { useState, type FormEvent, type ChangeEvent } from "react";
+
+export default function TodoInsert({ onAdd } : { onAdd : (text: string) => void }){
+    const [value, setValue] = useState("");
+
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setValue(e.target.value);
+    };
+
+    const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault(); // 페이지 새로고침 방지
+        const text = value.trim();
+        if (!text) return;
+        onAdd(text);
+    };
+    return (
+        <>
+            <form onSubmit={onSubmit}>
+                <input
+                    type="text"
+                    onChange={onChange}
+                    value={value}
+                    placeholder="할 일을 입력하고 Enter"
+                />
+                <button type="submit">추가</button>
+            </form>
+        </>
+    );
+}
